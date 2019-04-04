ab4c96ea8726bd187d0696eb406a3d9cd8ce0369
Notascamtrustme <internetaccess4jason@gmail.com>
2019-04-05 10:23:10 +1100

    Updated new section

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@
 
 # This is a python test
 print ("Hello World")
+
+# This is a new section
+
+name = input("What us your name?)
+if name == "Jason":
+	print("You Rock!!!")
+else:
+	print("You Suck!!!")
